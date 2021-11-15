82c06b0db255d429fc99bf021db5c1dbeeef0a5c
Inokcode <regilearn@gmail.com>
2021-11-15 20:27:00 +0530

    NoteList added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
+import Header from './components/Header';
+import NotesList from './components/NotesList';
 import { Note } from './models/note.model'
 
 
 
 function App() {
-  const [state, setstate] = useState<Note[]>([{
+  const [notes, setNotes] = useState<Note[]>([{
     id: (new Date).toString(),
     title: "God bless you",
     text: "Schedule meeting with UI/UX Team",
@@ -13,9 +16,16 @@ function App() {
     date: (new Date).toString(),
   }])
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Header />
+      <Container className='mt-5'>
+        <Row>
+          <Col>
+            <NotesList notes={notes} />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Notes.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Note } from '../models/note.model';
+
+interface INotesProps {
+    note: Note
+}
+
+const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
+    return (<div>Note card</div>);
+};
+
+export default Notes;
